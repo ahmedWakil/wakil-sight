734c053cd5f8997e0f6a8311084d3364b0cd1723
ahmedWakil <raiyan33380@gmail.com>
2023-06-01 18:41:21 -0400

    added in a background for better contrast with text

--- src/components/body/IntroBlock.jsx
+++ src/components/body/IntroBlock.jsx
@@ -18,22 +18,24 @@ function IntroBlock() {
 
   return (
     <article className="intro-container">
-      <Fade
-        cascade
-        duration={500}
-        keyframes={customAnimation}
-      >
+      <div className='intro-block-background'>
+        <Fade
+          cascade
+          duration={500}
+          keyframes={customAnimation}
+        >
 
-        <p>Hi, my name is</p>
-        <h1 className="intro-name">Wakil Ahmed</h1>
-        <h2 className="intro-profession">Building moving web applications</h2>
-        <p> I have a wide veriety of interests and experience in Data Science,
-          Machine Learning and Software Engineering.
+          <p>Hi, my name is</p>
+          <h1 className="intro-name">Wakil Ahmed</h1>
+          <h2 className="intro-profession">Building moving web applications</h2>
+          <p> I have a wide veriety of interests and experience in Data Science,
+            Machine Learning and Software Engineering.
 
-          I am passionate about taking my interdisciplinary expertise and
-          applying them in new ways to help build something useful.
-        </p>
-      </Fade>
+            I am passionate about taking my interdisciplinary expertise and
+            applying them in new ways to help build something useful.
+          </p>
+        </Fade>
+      </div>
     </article>
   )
 }
